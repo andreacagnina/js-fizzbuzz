f97674d1f61237886f4161a3d55e7cd0832550c0
andreacagnina <amirkal@libero.it>
2024-06-27 15:37:41 +0200

    [FEAT] completed 3rd condition + mod in console description to fizz buzz fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 for (let i = 1; i <= 100; i++) {
     console.log(i)
-    if (i % 3 === 0) {
-        console.log(i + 'è un numero divisibile per 3')
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i + ' FizzBuzz')
+    }
+    else if (i % 3 === 0) {
+        console.log(i + ' Buzz')
     }
     else if (i % 5 === 0) {
-        console.log(i + 'è un numero divisibile per 5')
+        console.log(i + ' Fizz')
     }
+
 }
