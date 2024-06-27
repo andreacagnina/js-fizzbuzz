d3349b27009c56890c6d0b949c338e8028ea139f
andreacagnina <amirkal@libero.it>
2024-06-27 18:26:23 +0200

    [FEAT] ADDED STYLING CLASS - COMPLETED

--- js/script.js
+++ js/script.js
@@ -7,12 +7,23 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 3 === 0 && i % 5 === 0) {
         content.innerText = 'FizzBuzz'
+        content.classList.add("bg-info");
+        content.classList.add("text-white");
+        content.classList.add("border-out");
+
+
     }
     else if (i % 3 === 0) {
         content.innerText = 'Buzz'
+        content.classList.add("bg-success");
+        content.classList.add("text-white");
+        content.classList.add("border-out");
     }
     else if (i % 5 === 0) {
         content.innerText = 'Fizz'
+        content.classList.add("bg-warning");
+        content.classList.add("text-white");
+        content.classList.add("border-out");
     }
 
     container.append(content)
